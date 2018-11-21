c7a4d1f035dea1cfb4d2598184b3308f1e5191d4
Matheus Bai <mathios@Matheuss-MacBook-Pro.local>
2018-11-21 13:49:36 -0500

    add x-axis feature

--- src/js/barchart.js
+++ src/js/barchart.js
@@ -1,10 +1,12 @@
+// Function that draws a bar chart for each element in data, style based on a option object and appends to a provided element
 function drawBarChart(data, option, element) {
   var chart = $("<ul class='chart'></ul>");
   var labels = $("<ul class='labels'></ul>");
   var title = $("<h3 class='title'></h3>");
   var chartLabels = $("<div class='chart-labels'></div>");
   var chartContainer = $("<div class='chart-container'></div>");
-  
+  var xAxis = $("<div class='x-axis'></div>");
+
   // Set CSS option for title
   title.css("color", option.fontColour);
   title.css("fontSize", option.fontSize);
@@ -21,13 +23,23 @@ function drawBarChart(data, option, element) {
   for (var i = 0; i < data.length; i++) {
     var item = $("<li></li>").text(data[i]);
     var label = $("<li></li>").text(option.labels[i]);
+    // Set item width according to the biggest value of data
+    var barWidth = (data[i] / option.xAxis) * 100 + "%";
     item.css("textAlign", option.textPosition);
     item.css("backgroundColor", option.barColor);
     item.css("marginBottom", option.barSpacing);
-    item.css("width", data[i] * 10 + "%");
+    item.css("width", barWidth);
     chart.append(item);
     labels.append(label);
   }
+
+  // Set an appropriate x-axis values according to the biggest data value
+  var xValue = (option.xAxis / 4).toFixed(2);
+  for (var j = 0; j < 5; j++) {
+    var xValueElem = $("<p></p>").text(xValue * j);
+    xAxis.append(xValueElem);
+  }
+
   // Add chart and labels ul to its respect div
   chartContainer.append(chart);
   chartLabels.append(labels);
@@ -35,13 +47,16 @@ function drawBarChart(data, option, element) {
   // Add div to element
   element.append(chartLabels);
   element.append(chartContainer);
+  element.append(xAxis);
 }
 
 
+// ==============================
 // Test Code
+// ==============================
 var element = $("div.container");
 
-var data = [6, 9, 5];
+var data = [3, 6, 9];
 
 var option = {
   title: "Favourite Programming Language",
@@ -51,7 +66,9 @@ var option = {
   textPosition: "left",
   barColor: "lightpink",
   barSpacing: "40px",
-  barChartAxes: "",
+  xAxis: data.reduce(function(a, b) {
+    return Math.max(a, b);
+  }),
   fontSize: "32px",
   fontColour: "red"
 };
