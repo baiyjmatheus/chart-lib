b75b73f0955b0195a16cf4e340c683c653420b13
Matheus Bai <mathios@Matheuss-MacBook-Pro.local>
2018-11-21 15:32:12 -0500

    adds multiple stacked bar chart and sum of multidimensional arrays

--- src/js/barchart.js
+++ src/js/barchart.js
@@ -1,3 +1,20 @@
+// Returns sum of the array, including multidimensional arrays
+function arraySum(array) {
+  var total = 0;
+  // iterate through array elements
+  array.forEach(function(arrElement) {
+    // checks if array element is an array
+    if (Array.isArray(arrElement)) {
+      // if it's in an array then recursively call arrSum to get its total;
+      total += arraySum(arrElement);
+    } else {
+      // else adds to the total
+      total += arrElement;
+    }
+  });
+  return total;
+}
+
 // Function that draws a bar chart for each element in data, style based on a option object and appends to a provided element
 function drawBarChart(data, option, element) {
   var chart = $("<ul class='chart'></ul>");
@@ -21,14 +38,38 @@ function drawBarChart(data, option, element) {
   
   // Create a new item for each element in array && add the correspondent label
   for (var i = 0; i < data.length; i++) {
-    var item = $("<li></li>").text(data[i]);
-    var label = $("<li></li>").text(option.labels[i]);
+    var item = $("<li></li>");
+    var label = $("<li></li>");
     // Set item width according to the biggest value of data
-    var barWidth = (data[i] / option.xAxis) * 100 + "%";
-    item.css("textAlign", option.textPosition);
-    item.css("backgroundColor", option.barColor);
+    var barWidth;
+    if (Array.isArray(data[i])) {
+      barWidth = (arraySum(data[i]) / option.xAxis) * 100 + "%";
+    } else {
+      barWidth = (data[i] / option.xAxis) * 100 + "%";
+    }
+
     item.css("marginBottom", option.barSpacing);
     item.css("width", barWidth);
+
+    if (Array.isArray(data[i])) {
+      var totalValue = arraySum(data[i]);
+
+      item.css("padding", "0");
+
+      for (var k = 0; data[i][k]; k++) {
+        var subBar = $("<span class='sub-bar'></span>").text(data[i][k]);
+        var subBarWidth = (data[i][k] / totalValue) * 100 + "%";
+        subBar.css("width", subBarWidth);
+        subBar.css("backgroundColor", option.multipleColors[k % option.multipleColors.length]);
+        item.append(subBar);
+      }
+    } else {
+      item.text(data[i]);
+      item.css("textAlign", option.textPosition);
+      item.css("backgroundColor", option.barColor);
+    }
+    label.text(option.labels[i]);
+
     chart.append(item);
     labels.append(label);
   }
@@ -56,21 +97,35 @@ function drawBarChart(data, option, element) {
 // ==============================
 var element = $("div.container");
 
-var data = [3, 6, 9];
+var data = [[1, 2, 3], [2, 3, 4], 5];
 
 var option = {
-  title: "Favourite Programming Language",
+  title: "Favourite JS Framework",
   labels: ["Angular", "React", "Vue.js"],
-  width: "960px",
+  width: "700px",
   height: data.length * 80 + "px",
   textPosition: "left",
   barColor: "lightpink",
   barSpacing: "40px",
   xAxis: data.reduce(function(a, b) {
-    return Math.max(a, b);
+    var aTotal = 0;
+    var btotal = 0;
+    if (Array.isArray(a)) {
+      aTotal = arraySum(a);
+    } else {
+      aTotal = a;
+    }
+
+    if (Array.isArray(b)) {
+      bTotal = arraySum(b);
+    } else {
+      bTotal = b;
+    }
+    return Math.max(aTotal, bTotal);
   }),
   fontSize: "32px",
-  fontColour: "red"
+  fontColour: "red",
+  multipleColors: ["lightpink", "lightblue", "lightgreen"]
 };
 
 drawBarChart(data, option, element);
